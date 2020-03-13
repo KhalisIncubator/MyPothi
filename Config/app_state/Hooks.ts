5ddcae490637ca874442ee52e6a7dc2837786281
akal-ustatsingh <akalustat.singh@gmail.com>
2020-03-12 22:24:54 -0700

    remove contexts

--- Config/app_state/Hooks.ts
+++ Config/app_state/Hooks.ts
@@ -1,22 +1,7 @@
-import { useMemo, useState } from 'react';
 import shallowEqual from 'shallowequal';
 import { useMainStoreState, useMainStoreActions } from './easy-peasy/hooks';
-/**
- *
- * @param apiFactory a factory that produces all the functions for an 'api' (container for context values)
- * @param initialState state to be used on first app load
- */
-const useApi = ( apiFactory, initialState ) => {
-  let [ state, setState ] = useState( initialState );
-  // only regenerate the factory when the state changes
-  return useMemo( () => apiFactory( { state, setState } ), [
-    state,
-    setState,
-    apiFactory,
-  ] );
-};
 
 const useValues = ( model: string ) => useMainStoreState( ( store ) => ( { ...store[model] } ), shallowEqual );
 const useUpdaters = ( model: string ) => useMainStoreActions( ( actions ) => ( { ...actions[model] } ) );
 
-export { useApi, useValues, useUpdaters };
+export { useValues, useUpdaters };
