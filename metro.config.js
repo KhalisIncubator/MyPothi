b84323a1f3d9f59b629a72b5caeb0769794dbd79
akal-ustatsingh <akalustat.singh@gmail.com>
2020-03-15 17:50:53 -0700

    add app icon

--- metro.config.js
+++ metro.config.js
@@ -6,15 +6,15 @@
  */
 
 module.exports = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: false,
-            },
-        }),
-    },
-    resolver: {
-        sourceExts: ['jsx', 'js', 'ts', 'tsx'],
-    },
+  transformer: {
+    getTransformOptions: async () => ( {
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    } ),
+  },
+  resolver: {
+    sourceExts: [ 'jsx', 'js', 'ts', 'tsx' ],
+  },
 };
