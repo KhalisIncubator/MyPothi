33a3e9c61613d8016c9e2293f828eb4db670735f
akal-ustatsingh <akalustat.singh@gmail.com>
2020-04-01 10:09:43 -0700

    update to realm 5.0.3

--- src/Functions.tsx
+++ src/Functions.tsx
@@ -1,10 +1,8 @@
 import { getModWithParent } from './database/LocalDatabase';
 // bug with realm where on first load this stuff is not an array for some reason
 
-const mapModsToArray = ( mods ) => ( mods ? Array.from( { ...mods, length: Object.keys( mods ).length } ) : [] );
 
 const mapToArray = ( obj ) => ( obj ? Array.from( { ...obj, length: Object.keys( obj ).length } ) : [] );
-
 export default mapToArray;
 // get current font size on selected element
 const getCurrentFontSize = ( [ lineid, element, parentID ], globalFontSize ) => {
@@ -14,4 +12,4 @@ const getCurrentFontSize = ( [ lineid, element, parentID ], globalFontSize ) => 
   }
   return null;
 };
-export { mapModsToArray, getCurrentFontSize };
+export { getCurrentFontSize };
