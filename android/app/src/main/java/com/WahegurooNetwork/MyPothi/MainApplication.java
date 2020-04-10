0d91a684d2af6043c09d814abf2ee85c622fda3b
akal-ustatsingh <akalustat.singh@gmail.com>
2020-04-10 09:35:47 -0700

    fix issues with builds

--- android/app/src/main/java/com/WahegurooNetwork/MyPothi/MainApplication.java
+++ android/app/src/main/java/com/WahegurooNetwork/MyPothi/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.mypothi.BuildConfig;
+import com.WahegurooNetwork.MyPothi.BuildConfig;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
@@ -65,7 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.mypothi.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.WahegurooNetwork.MyPothi.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
