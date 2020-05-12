a03f492ebbf77803813677de5de2540109faeff0
akal-ustatsingh <akalustat.singh@gmail.com>
2020-05-12 13:21:27 -0700

    [chore]: add deprication

--- src/store/StoreConsts.js
+++ src/store/StoreConsts.js
@@ -1,9 +1,17 @@
+/**
+ * @deprecated
+ */
+
 const FontConsts = {
   gurmukhi: 'Gurmukhi',
   eng: 'Translation',
   teeka: 'Teeka',
   translit: 'Transliteration',
 };
+
+/**
+ * @deprecated
+ */
 
 const DisplayConsts = {
   displayEng: 'English',
