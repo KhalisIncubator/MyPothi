ef398190558136aef667ac59469c5518ad6f16a7
akal-ustatsingh <akalustat.singh@gmail.com>
2020-05-22 09:30:17 -0700

    [fix]: android OpenGurbaniFont not working

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 [![Language grade: JavaScript](https://img.shields.io/lgtm/grade/javascript/g/KhalisIncubator/MyPothi.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/KhalisIncubator/MyPothi/context:javascript)
 
 create custom pothis
+
+
+// @bam.tech/react-native-make for the icon
--- src/components/main/text/TextContainer.tsx
+++ src/components/main/text/TextContainer.tsx
@@ -2,26 +2,38 @@
 import React, { ReactNode } from 'react';
 import { Results } from 'realm';
 import {
-  Text, StyleSheet, TextStyle, StyleProp,
+  Text, StyleSheet, TextStyle, StyleProp, Platform,
 } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { Modification } from '../../../../types/types';
 
 
+const generateFont = ( isGurmukhi, isBold ) => {
+  if ( Platform.OS === 'android' ) {
+    if ( isGurmukhi ) return isBold ? ( { fontFamily: 'OpenGurbaniAkhar-Bold' } ) : ( { fontFamily: 'OpenGurbaniAkhar-Regular' } );
+    if ( isBold ) return ( { fontWeight: 'bold' } );
+  } else {
+    if ( isBold ) return ( { fontWeight: 'bold' } );
+    if ( isGurmukhi ) return ( { fontFamily: 'OpenGurbaniAkhar' } );
+  }
+
+  return null;
+};
+
 const modMap = {
   backgroundColor: ( value ) => ( { backgroundColor: value } ),
-  bold: ( value ) => ( value ? ( { fontWeight: 'bold' } ) : null ),
+  bold: ( value, isGurmukhi ) => ( value ? generateFont( isGurmukhi, true ) : null ),
   fontSize: ( value ) => ( value ? ( { fontSize: value } ) : null ),
 
 };
 
-const generateStyle = ( modResults, style ) => {
+const generateStyle = ( modResults, style, isGurmukhi ) => {
   const { 0: mod } = modResults;
   const modStyle = mod?.entries().reduce( ( acc, [ key, value ] ) => {
     const createrFunc = modMap?.[key];
     return ( {
       ...acc,
-      ...( createrFunc ? createrFunc( value ) : {} ),
+      ...( createrFunc ? createrFunc( value, isGurmukhi ) : {} ),
     } );
   }, {} ) ?? {};
 
@@ -31,14 +43,17 @@ const generateStyle = ( modResults, style ) => {
 
 
 interface TextContainerProps {
-  style?: StyleProp<TextStyle>
+  style?: any,
   mod?: Results<Modification>,
   children?: ReactNode
+  isGurmukhi?: boolean
 }
 
-const RomanTextContainer: React.FC<TextContainerProps> = ( { style, children, mod } ) => {
+const RomanTextContainer: React.FC<TextContainerProps> = ( {
+  style, children, mod, isGurmukhi = false,
+} ) => {
   const theme = useTheme();
-  const textStyle = generateStyle( mod, StyleSheet.flatten( [ style, { color: theme.colors.text }, styles.Text ] ) );
+  const textStyle = generateStyle( mod, StyleSheet.flatten( [ style, { color: theme.colors.text }, styles.Text ] ), isGurmukhi );
   return (
     <Text style={textStyle}>
       {children}
@@ -46,7 +61,7 @@ const RomanTextContainer: React.FC<TextContainerProps> = ( { style, children, mo
   );
 };
 const GurmukhiTextContainer: React.FC<TextContainerProps> = ( { style, children, mod } ) => (
-  <RomanTextContainer style={StyleSheet.flatten( [ { fontFamily: 'OpenGurbaniAkhar' }, style ] )} mod={mod}>
+  <RomanTextContainer style={StyleSheet.flatten( [ generateFont( true, false ), style ] )} mod={mod} isGurmukhi>
     {children}
   </RomanTextContainer>
 );
@@ -57,6 +72,5 @@ const styles = StyleSheet.create( {
     paddingVertical: 2,
   },
 } );
-
 
 export { RomanTextContainer, GurmukhiTextContainer };
